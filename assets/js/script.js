fcb7b513dd73823d905add4e49c689a3f89d11c9
ismail alif <43023388+alif33@users.noreply.github.com>
2024-01-04 22:42:01 +0600

    testimonial swiper init

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,20 @@ const showMenu = () => {
     sidebar.style.display = "block"
 }
 
-const swiper = new Swiper('.swiper-container', {
-    // Optional: Add more Swiper configurations here
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-});
+  
+  });
